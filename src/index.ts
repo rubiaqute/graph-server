fb9484552c7365b853ed15a67b2af5e04d334e8e
rubiaqute <rubiaqute@gmail.com>
2022-06-30 11:49:40 +0300

    init: initial folders creating

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { ApolloServer, gql } from "apollo-server";
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cache: "bounded",
+});
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
